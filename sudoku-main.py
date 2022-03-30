a44ce1a095866aee5bd7c9ff8ba4861645d2862c
Lexi Hattaway <lexhatt1@gmail.com>
2022-03-30 14:51:34 -0500

    added a board outline!
    shifted offsets and things to align everything. still hardcoded tho

--- sudoku-main.py
+++ sudoku-main.py
@@ -46,14 +46,14 @@ class Tile(pygame.sprite.Sprite):
         self.image, self.rect = load_image(self.image_name, -1)
         self.boardx = boardx 
         self.boardy = boardy
-        self.screenx = boardx*64
-        self.screeny = boardy*64
 
     def __str__(self):
         return "VALUE=%s, IMAGENAME=%s, bx=%s, by=%s" % (self.value, self.image_name, self.boardx, self.boardy) 
 
     def draw(self, screen):
-        screen.blit(self.image, (self.screenx, self.screeny))
+        screenx = 3 + self.boardx*65 + int(self.boardx / 3) * 2
+        screeny = 3 + self.boardy*65 + int(self.boardy / 3) * 2
+        screen.blit(self.image, (screenx, screeny))
 
     def modify(self, new_value):
         self.value = new_value
@@ -70,7 +70,9 @@ class Cursor(pygame.sprite.Sprite):
         self.image, self.rect = load_image("cursor.png", (0,255,0))
 
     def draw(self, screen):
-        screen.blit(self.image, (self.board_pos[0] * 64, self.board_pos[1] * 64))
+        screenx = 3 + self.board_pos[0]*65 + int(self.board_pos[0] / 3) * 2
+        screeny = 3 + self.board_pos[1]*65 + int(self.board_pos[1] / 3) * 2
+        screen.blit(self.image, (screenx, screeny))
         
     def move(self, x_movement, y_movement):
         self.board_pos[0] += x_movement
@@ -87,6 +89,7 @@ class Board(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.sudoku_board = self._init_board() # a 2d array board full of Tile objects
         self.cursor = Cursor()
+        self.image, self.rect = load_image("board.png", (255,0,255))
 
     def _init_board(self):
         assembling_board = []
@@ -108,6 +111,7 @@ class Board(pygame.sprite.Sprite):
                 print(tile)
 
     def draw(self, screen):
+        screen.blit(self.image, (0,0))
         for row in self.sudoku_board:
             for tile in row:
                 tile.draw(screen)
