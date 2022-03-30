d91ead15cee5a18684759c6a432facbf24e65b10
Lexi Hattaway <lexhatt1@gmail.com>
2022-03-30 11:09:17 -0500

    hacking out the rough edges
    the board is displaying now!

--- sudoku-main.py
+++ sudoku-main.py
@@ -4,7 +4,7 @@ main_dir = os.path.split(os.path.abspath(__file__))[0]
 data_dir = os.path.join(main_dir, "data")
 
 def init_board():
-    one_row = [0,0,0, 0,0,0, 0,0,0]
+    one_row = [1,2,3, 4,5,6, -1,8,9]
     sudoku_board = []
     for _ in range(0,9):
         sudoku_board.append(one_row)
@@ -24,7 +24,7 @@ def load_image(name, colorkey=None, scale=1):
 
     size = image.get_size()
     size = (size[0] * scale, size[1] * scale)
-    image = pygame.transform.scale(image.size)
+    image = pygame.transform.scale(image, size)
 
     image = image.convert()
     if colorkey is not None:
@@ -37,10 +37,55 @@ def load_image(name, colorkey=None, scale=1):
 class Tile(pygame.sprite.Sprite):
     """all the tiles"""
 
-    def __init__(self):
+    def __init__(self, value=-1, boardx=0, boardy=0):
         pygame.sprite.Sprite.__init__(self) # calls parent Sprite initializer
-        self.image, self.rect = load_image("blue1.png", -1)
+        self.value = value
+        self.image_name = "empty.png"
+        if value != -1:
+            self.image_name = "blue" + str(value) + ".png"
+        self.image, self.rect = load_image(self.image_name, -1)
+        self.boardx = boardx 
+        self.boardy = boardy
+        self.screenx = boardx*64
+        self.screeny = boardy*64
 
+    def __str__(self):
+        return "VALUE=%s, IMAGENAME=%s, bx=%s, by=%s" % (self.value, self.image_name, self.boardx, self.boardy) 
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.screenx, self.screeny))
+
+
+class Board(pygame.sprite.Sprite):
+    """contains all tiles"""
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.sudoku_board = self._init_board() # a 2d array board full of Tile objects
+
+    def _init_board(self):
+        assembling_board = []
+
+        temp_board = init_board()
+        row_counter = 0
+        for j, row in enumerate(temp_board):
+            assembling_row = []
+            for i, value in enumerate(row):
+                assembling_row.append(Tile(value, i, j))
+
+            assembling_board.append(assembling_row)
+
+        return assembling_board
+
+
+    def print_board(self):
+        for row in self.sudoku_board:
+            for tile in row:
+                print(tile)
+
+    def draw(self, screen):
+        for row in self.sudoku_board:
+            for tile in row:
+                tile.draw(screen)
 
 def main():
 
@@ -54,16 +99,60 @@ def main():
     black = 0,0,0
 
     screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("pydoku")
+
+
+    ############
+    #init
+    ############
+
+    #num1 = Tile()
+    #allsprites = pygame.sprite.RenderPlain((num1))
+
+    board = Board()
+
+    background = pygame.Surface(screen.get_size())
+
+    if pygame.font:
+        font = pygame.font.Font(None, 64)
+        text = font.render("Sudoku Gaming :)", True, (10,10,10))
+        textpos = text.get_rect(centerx=background.get_width() / 2, y=10)
+
+    clock = pygame.time.Clock()
 
     gamelooprunning = True
     while gamelooprunning:
+        clock.tick(60)
+        #########
+        # inputs and event handling
+        #########
         for event in pygame.event.get():
-            print(event)
+            #print(event)
             #if event.type == pygame.KeyDown: 
-            if event.type == pygame.QUIT: gamelooprunning=False
+            if event.type == pygame.QUIT: 
+                gamelooprunning=False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE: 
+                gamelooprunning=False
 
-        screen.fill(black)
+        #########
+        # draw
+        #########
+        background = pygame.Surface(screen.get_size())
+        background = background.convert()
+        background.fill(black)
+        #screen.fill(black)
+
+        background.blit(text, textpos)
+
+        #allsprites.update()
+
+        screen.blit(background, (0,0))
+
+        board.draw(screen)
+        #allsprites.draw(screen)
         pygame.display.flip()
+
+        board.print_board()
 
     pygame.quit()
 
